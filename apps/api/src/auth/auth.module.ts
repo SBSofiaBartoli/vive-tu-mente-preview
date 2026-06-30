75c098c3544e5817c3a5a689f07acd7958b14463
SofiaBartoli <sofiadbartoli@gmail.com>
2026-06-30 16:34:56 -0300

    feat: agrego control de roles administrativos

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { SupabaseModule } from '../supabase/supabase.module';
 import { AuthService } from './auth.service';
 import { AdminAuthGuard } from './admin-auth.guard';
 import { AuthController } from './auth.controller';
+import { AdminRolesGuard } from './admin-roles.guard';
 
 @Module({
   imports: [SupabaseModule],
   controllers: [AuthController],
-  providers: [AuthService, AdminAuthGuard],
-  exports: [AuthService, AdminAuthGuard],
+  providers: [AuthService, AdminAuthGuard, AdminRolesGuard],
+  exports: [AuthService, AdminAuthGuard, AdminRolesGuard],
 })
 export class AuthModule {}
