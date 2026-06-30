fe51885214221dd0518beb351fcd3c7e6e592549
SofiaBartoli <sofiadbartoli@gmail.com>
2026-06-30 16:27:53 -0300

    feat: agrego endpoint protegido de administrador

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { SupabaseModule } from '../supabase/supabase.module';
 import { AuthService } from './auth.service';
 import { AdminAuthGuard } from './admin-auth.guard';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [SupabaseModule],
+  controllers: [AuthController],
   providers: [AuthService, AdminAuthGuard],
   exports: [AuthService, AdminAuthGuard],
 })
