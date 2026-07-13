8854d3f09333c137de8dc4027b5c425344a4c341
SofiaBartoli <sofiadbartoli@gmail.com>
2026-07-13 20:59:03 -0300

    fix: agrego modulo de auth en recursos protegidos

--- apps/api/src/content/content.module.ts
+++ apps/api/src/content/content.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SupabaseModule } from '../supabase/supabase.module';
 import { ContentController } from './content.controller';
 import { ContentService } from './content.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [SupabaseModule],
+  imports: [SupabaseModule, AuthModule],
   controllers: [ContentController],
   providers: [ContentService],
 })
--- apps/api/src/media-files/media-files.module.ts
+++ apps/api/src/media-files/media-files.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SupabaseModule } from '../supabase/supabase.module';
 import { MediaFilesController } from './media-files.controller';
 import { MediaFilesService } from './media-files.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [SupabaseModule],
+  imports: [SupabaseModule, AuthModule],
   controllers: [MediaFilesController],
   providers: [MediaFilesService],
 })
